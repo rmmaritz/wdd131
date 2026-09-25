57b94a733f12c7e813c0fbedca5934ed65f88219
rmmaritz <roconnellmaritz@byupathway.edu>
2026-09-25 16:51:51 +0200

    Additional meta to overcome the third party cookies

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -5,7 +5,7 @@ const temples = [
     dedicated: "2005, August, 7",
     area: 11500,
     imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+      "https://churchofjesuschristtemples.org/assets/img/temples/aba-nigeria-temple/aba-nigeria-temple-8211.jpg"
   },
   {
     templeName: "Manti Utah",
@@ -45,7 +45,7 @@ const temples = [
     dedicated: "1986, January, 10",
     area: 9600,
     imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+      "https://churchofjesuschristtemples.org/assets/img/temples/lima-peru-temple/lima-peru-temple-12726.jpg"
   },
   {
     templeName: "Mexico City Mexico",
